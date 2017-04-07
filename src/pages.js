d076d85987bfd0f501371ae733f840bb4d0a1be5
[a526177] Sylvain Pollet-Villard <sylvain.polletvillard@worldline.com>
2017-04-07 18:06:36 +0200

    more keywords

--- src/pages.js
+++ src/pages.js
@@ -14,12 +14,12 @@ export const pages = {
 				{
 					title: "Qu'est-ce qu'une PWA ?",
 					link: "pwa",
-					keywords: "pwa;progressive;webapp;kesako;quoi"
+					keywords: "pwa;progressive;webapp;kesako;quoi;intro;concept"
 				},
 				{
 					title: "Exemples de PWA",
 					link: "examples",
-					keywords: "pwa;exemples;démo;client;poc",
+					keywords: "pwa;exemples;démo;client;poc;test;essayer",
 					component: ExamplesPage
 				},
 				{
@@ -35,12 +35,12 @@ export const pages = {
 				{
 					title: "Réduire la taille de l'application",
 					link: "optimisation-bundle",
-					keywords:"optimiser;réduire;bundle;taille;lourd;minif;bande-passante;débit"
+					keywords:"optimiser;réduire;bundle;paquet;charge;perf;taille;lourd;minif;débit"
 				},
 				{
 					title: "Utiliser efficacement le cache client",
 					link: "cache-client",
-					keywords:"optimiser;économiser;cache;storage;client;enregistrer;bande-passante;débit"
+					keywords:"optimiser;économiser;cache;storage;client;enregistrer;débit"
 				},
 				{
 					title: "Usage hors-ligne et Service Workers",
@@ -55,22 +55,22 @@ export const pages = {
 				{
 					title: "Intégration avec les plates-formes",
 					link: "integration",
-					keywords:"intégration;mobile;application;installation;notification;engagement;publicité;store;appstore"
+					keywords:"intégration;mobile;application;installation;notification;engagement;publicité;store;plate-forme"
 				},
 				{
 					title: "Manifeste Web App",
 					link: "manifest",
-					keywords:"manifeste;recherche;référencement;mobile;engagement"
+					keywords:"manifeste;recherche;référencement;mobile;engagement;méta"
 				},
 				{
 					title: "SEO: Optimiser le référencement",
 					link: "seo",
-					keywords:"seo;recherche;indexation;moteur;lien;référencement"
+					keywords:"seo;recherche;indexation;moteur;lien;référencement;google"
 				},
 				{
 					title: "Notifications Push",
 					link: "push-notifications",
-					keywords:"notification;push;engagement;publicité"
+					keywords:"notification;push;engagement;publicité;natif"
 				},
 			]
 		},
@@ -86,7 +86,7 @@ export const pages = {
 				{
 					title: "Outillage d'audit",
 					link: "audit-tools",
-					keywords:"outillage;audit;vérifier;contrôler;lighthouse"
+					keywords:"outillage;outils;audit;vérifier;contrôler;lighthouse;qualité"
 				}
 			]
 		}
